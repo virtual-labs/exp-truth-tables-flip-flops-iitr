989d3a45487895b1acb5b7e69863db36eb060921
vlab-iitr <vlabiitr.gmail.com>
2020-12-03 10:33:24 +0530

    add

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
-## Introduction
+## Introduction (Round 0)
 
 
-<b>Discipline | <b>Fill your discipline name here
+<br>
+
+<b>Discipline | <b> Engineering
 :--|:--|
-<b> Lab | <b> Fill your lab name here
-<b> Experiment|     <b> Fill your experiment name and number here
+<b> Lab | <b> Digital Electronics
+<b> Experiment|     <b> 6. Verify the truth table of RS, JK, T and D flip-flops using NAND & NOR gates.
 
-### About the Experiment 
+<h5> About the Experiment : </h5> <br>
 
-Fill a brief description of this experiment here
+This experiment is to verify the truth table and timing diagram of RS, JK, T and D flip-flops by using NAND & NOR gates ICs and analyse the circuit of RS, JK, T and D flip-flops with the help of LEDs display.
 
-<b>Name of Developer | <b> Fill the name of experiment owner here 
+<b>Name of Developer | <b> R.S. Anand
 :--|:--|
-<b> Institute | <b>  
-<b> Email id|     <b>  
-<b> Department |  
+<b> Institute | <b> IIT Roorkee
+<b> Email id|     <b> anandfee@gmail.com
+<b> Department | Electrical Engineering
 
-### Contributors List
+#### Contributors List
 
 SrNo | Name | Faculty or Student | Department| Institute | Email id
 :--|:--|:--|:--|:--|:--|
-1 | . | . | . | . | .
-2 | . | . | . | . | .
+1 | R.S. Anand | Faculty | Electrical Engineering | IIT Roorkee, Roorkee | anandfee@gmail.com
+2 | Jasbir Singh | Research Fellow | Electrical Engineering | IIT Roorkee, Roorkee | jasbirjassy6@gmail.com 
+3 | Rajeev Kumar | Research Fellow | Electrical Engineering | IIT Roorkee, Roorkee | rajeevkumar.rke@gmail.com
+4 | Priyanshi Agarwal | Research Fellow | Electrical Engineering | IIT Roorkee, Roorkee | priyanshi.a07@gmail.com
+
+
+<br>
---
+++ storyboard/mindmap/mindmap.mm
@@ -0,0 +1,128 @@
+<map version="freeplane 1.7.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Verify the truth table of RS, JK, T and D flip-flops using NAND &amp; NOR gates." FOLDED="false" ID="ID_1943786200" CREATED="1549540469642" MODIFIED="1579771545581" STYLE="oval">
+<font SIZE="19"/>
+<hook NAME="MapStyle" zoom="0.826">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<edge WIDTH="8"/>
+<hook URI="logo.jpg" SIZE="0.24220829" NAME="ExternalObject"/>
+<node TEXT="Cognitive Levels" POSITION="right" ID="ID_824686825" CREATED="1549542038685" MODIFIED="1577441522639">
+<edge COLOR="#00ff00"/>
+<node TEXT="Recall" ID="ID_910633405" CREATED="1549542190173" MODIFIED="1579770732345"/>
+<node TEXT="Apply" ID="ID_757096825" CREATED="1549542208300" MODIFIED="1579242831976"/>
+<node TEXT="Analyse" ID="ID_1290923315" CREATED="1579242856690" MODIFIED="1579770102227"/>
+</node>
+<node TEXT="Simulation" POSITION="left" ID="ID_1642475416" CREATED="1549542634653" MODIFIED="1579243424908" HGAP_QUANTITY="29.74999953061344 pt" VSHIFT_QUANTITY="-37.49999888241293 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Visuals" ID="ID_117560409" CREATED="1549542675873" MODIFIED="1579772738405" HGAP_QUANTITY="46.99999901652339 pt" VSHIFT_QUANTITY="-5.249999843537812 pt">
+<node TEXT="Circuit diagram using NAND gates IC." ID="ID_1220489218" CREATED="1549542818036" MODIFIED="1579772723569" HGAP_QUANTITY="43.99999910593036 pt" VSHIFT_QUANTITY="43.499998703599005 pt">
+<hook URI="nand.png" SIZE="0.46036202" NAME="ExternalObject"/>
+</node>
+<node TEXT="Led glow for high output, switches for inputs and timing diagram for each state." ID="ID_14493876" CREATED="1549544423676" MODIFIED="1579772738400" HGAP_QUANTITY="35.7499993517995 pt" VSHIFT_QUANTITY="-11.249999664723887 pt"/>
+<node TEXT="For realization of truth table of flip flop using NAND gate IC." ID="ID_194157097" CREATED="1577441988274" MODIFIED="1579772598348" HGAP_QUANTITY="18.499999865889546 pt" VSHIFT_QUANTITY="-37.499998882412925 pt"/>
+</node>
+<node TEXT="Pathway" ID="ID_356329700" CREATED="1549542688402" MODIFIED="1579243424907" HGAP_QUANTITY="178.99999508261692 pt" VSHIFT_QUANTITY="109.49999673664581 pt">
+<node TEXT="Click" ID="ID_93255511" CREATED="1549544299322" MODIFIED="1579243361106">
+<node TEXT="button" ID="ID_809208887" CREATED="1549544395400" MODIFIED="1579243361105" HGAP_QUANTITY="4.250000290572634 pt" VSHIFT_QUANTITY="-0.7499999776482589 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_194157097" STARTINCLINATION="156;0;" ENDINCLINATION="156;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Animation" ID="ID_1626191437" CREATED="1549544316264" MODIFIED="1578996219239">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_14493876" STARTINCLINATION="200;0;" ENDINCLINATION="200;0;" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+</node>
+</node>
+<node TEXT="Assesment" ID="ID_887544915" CREATED="1549542705615" MODIFIED="1578983767405" HGAP_QUANTITY="103.24999734014281 pt" VSHIFT_QUANTITY="7.499999776482596 pt">
+<node TEXT="Formative Assesment" ID="ID_920802616" CREATED="1549805618004" MODIFIED="1549805633309"/>
+<node TEXT="Pre Test" ID="ID_647252806" CREATED="1549805976344" MODIFIED="1549805998142"/>
+<node TEXT="Post Test" ID="ID_1765057062" CREATED="1549806015925" MODIFIED="1549806024690"/>
+</node>
+</node>
+<node TEXT="Strategy" POSITION="right" ID="ID_1544353669" CREATED="1549543085606" MODIFIED="1549806193582" HGAP_QUANTITY="8.000000178813927 pt" VSHIFT_QUANTITY="48.749998547136826 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="Guided Inquiry" ID="ID_350102762" CREATED="1549543098018" MODIFIED="1578917378406">
+<node TEXT="Recall the circuit of RS, JK, D and T flip flop  using NAND gate IC." ID="ID_1049223549" CREATED="1549543171135" MODIFIED="1579772528531"/>
+<node TEXT="Select one flip flop to perform the experiment." ID="ID_480837278" CREATED="1549543183081" MODIFIED="1579772388677"/>
+<node TEXT="Select the input combination and click on add button." ID="ID_304424191" CREATED="1549543196285" MODIFIED="1579770224217"/>
+<node TEXT="Verify the truth table and timing diagram for each possible set of combination." ID="ID_1196857867" CREATED="1549543229044" MODIFIED="1579770255452"/>
+</node>
+</node>
+<node TEXT="Introduction" POSITION="right" ID="ID_1651007277" CREATED="1549543443089" MODIFIED="1579770300237" HGAP_QUANTITY="15.499999955296516 pt" VSHIFT_QUANTITY="86.24999742954978 pt">
+<edge COLOR="#007c00"/>
+<node TEXT="Interference" ID="ID_1751555606" CREATED="1549543456367" MODIFIED="1549543466108"/>
+<node TEXT="Circuit diagram for all flip flop using of NAND gate IC." ID="ID_1732542503" CREATED="1549543469811" MODIFIED="1579772559900"/>
+<node TEXT="Realization of truth table and timing diagram for flip flops using NAND gate IC." ID="ID_1791927761" CREATED="1549543491643" MODIFIED="1579772574259"/>
+</node>
+<node TEXT="Information" POSITION="left" ID="ID_1213487111" CREATED="1549543585928" MODIFIED="1578983994117" HGAP_QUANTITY="110.7499971166254 pt" VSHIFT_QUANTITY="1.499999955296511 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="Domain" ID="ID_624832964" CREATED="1549543597783" MODIFIED="1549543805246">
+<node TEXT="Engineering" ID="ID_920968540" CREATED="1549543762121" MODIFIED="1549543793759"/>
+</node>
+<node TEXT="Course" ID="ID_1623950742" CREATED="1549543607438" MODIFIED="1549543614058">
+<node TEXT="Digital Electronics" ID="ID_1112392188" CREATED="1549543750692" MODIFIED="1580450926650"/>
+</node>
+<node TEXT="Focus" ID="ID_510849788" CREATED="1549543619590" MODIFIED="1549543809796">
+<node TEXT="Experimentation" ID="ID_138591745" CREATED="1549543695728" MODIFIED="1579243460783"/>
+</node>
+</node>
+</node>
+</map>
